42c77d580dfe0a265db94588ba023b20f1e6ab4a
Dixit Parkhiya <parkhiyadixit@gmail.com>
2024-10-14 22:19:58 +0530

    Custom delimiters, negative number restriction

--- TDD-Kata/index.js
+++ TDD-Kata/index.js
@@ -1,7 +1,15 @@
 function add(numbers){
     if(numbers==="") return 0;
     let delimeters = /,|\n/; //default comma or newline 
+    if(numbers.startsWith("//")){
+        const temp = numbers.match(/^\/\/(.)\n/);
+        delimeters = new RegExp(temp[1]);
+        numbers = numbers.slice(4);
+    }
     const numArr = numbers.split(delimeters).map(Number);
+    if(numArr.filter(i=>i<0).length > 0 ){
+        throw new Error("Negative Numbers not allowed");
+    }
     if(numArr.length === 1) return numArr[0];
     return numArr.reduce((sum,num) => sum + num, 0);
 }
@@ -19,17 +27,30 @@ function testAddTwoNumber(){
 }
 
 function testAddMultipleNumber(){
-    console.log(add("2,3,4,5"))
+    // console.log(add("2,3,4,5"))
     console.assert(add("2,3,4,5")==14, "Test Failed ! Invalid Sum ")
 }
 function testNewLineDelimeter(){
-    console.log(add("2\n3,4"))
+    // console.log(add("2\n3,4"))
     console.assert(add("2\n3,4")==9, "Test Failed ! Invalid Sum ")
+}
+function testCustomDelimites(){
+    console.assert(add("//~\n8~9")===17,`Test for Custom delimiters failed ${add("//~\n8~9")}`);
+}
+function testNegativeNums(){
+    try{
+        add("1,-1,2");
+    }catch (e) {
+        // console.log(e);
+        console.assert(e.message==="Negative Numbers not allowed","Failed ! should show negs are not allowed")
+    }
 }
 
 console.log("Working...")
-testEmptyString()
-testSingleNumber()
-testAddTwoNumber()
-testAddMultipleNumber()
-testNewLineDelimeter()
+// testEmptyString()
+// testSingleNumber()
+// testAddTwoNumber()
+// testAddMultipleNumber()
+// testNewLineDelimeter()
+testCustomDelimites()
+testNegativeNums()
